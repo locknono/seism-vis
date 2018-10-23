f4f476fbe0ad3e7d82ef6037e690f3eb4b71eaa0
locknono <guozy0103@outlook.com>
2018-10-23 17:00:52 +0800

    Add draw line annimation

---
+++ client/src/component/FigureSvgLayer.js
@@ -0,0 +1,25 @@
+import React, { Component } from "react";
+
+class FigureSvgLayer extends Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    const { width, height, className, lineCoors } = this.props;
+    const [x1, y1] = lineCoors[0];
+    const [x2, y2] = lineCoors[1];
+    return (
+      <svg
+        className={className}
+        width={width}
+        height={height}
+        pointerEvents="none"
+      >
+        <line x1={x1} y1={y1} x2={x2} y2={y2} stroke="black" />
+      </svg>
+    );
+  }
+}
+
+export default FigureSvgLayer;
--- client/src/component/MatrixFigure.js
+++ client/src/component/MatrixFigure.js
@@ -1,11 +1,16 @@
 import React, { Component } from "react";
 import PolylineSvg from "./PolylineSvg";
+import FigureSvgLayer from "./FigureSvgLayer";
 class MatrixFigure extends Component {
   constructor(props) {
     super(props);
     this.state = { zData: [] };
     this.figureRef = React.createRef();
     this.onClick = this.onClick.bind(this);
+    this.onMouseDown = this.onMouseDown.bind(this);
+    this.onMouseUp = this.onMouseUp.bind(this);
+    this.onMouseMove = this.onMouseMove.bind(this);
+    this.getXY = this.getXY.bind(this);
   }
   componentDidMount() {
     /**Browser loads img asynchronously,
@@ -23,14 +28,11 @@ class MatrixFigure extends Component {
   }
   onClick(e) {
     //x for 886,y for 716
-    
-    //TO-DO:store Global variables such as 
-    //row number ,vmin into `context` 
+
+    //TO-DO:store Global variables such as
+    //row number ,vmin into `context`
     if (this.props.plane !== "xy") return;
-    const { offsetX, offsetY } = e.nativeEvent;
-    const { width, height } = this.state;
-    const x = Math.floor((offsetX / width) * 886);
-    const y = Math.floor((offsetY / height) * 716);
+    const { x, y } = this.getXY(e);
     fetch(`http://localhost:5000/xy/${x}-${y}`)
       .then(response => response.json())
       .then(zData => {
@@ -38,17 +40,70 @@ class MatrixFigure extends Component {
       });
   }
 
+  onMouseDown(e) {
+    e.preventDefault();
+    this.mousedownFlag = true;
+    const { x, y } = this.getXY(e);
+    this.x1 = e.nativeEvent.offsetX;
+    this.y1 = e.nativeEvent.offsetY;
+  }
+  onMouseMove(e) {
+    if (this.mousedownFlag) {
+      this.x2 = e.nativeEvent.offsetX;
+      this.y2 = e.nativeEvent.offsetY;
+      this.setState({
+        x1: this.x1,
+        x2: this.x2,
+        y1: this.y1,
+        y2: this.y2
+      });
+    }
+  }
+  onMouseUp(e) {
+    this.mousedownFlag = false;
+    const { x, y } = this.getXY(e);
+    this.x2 = e.nativeEvent.offsetX;
+    this.y2 = e.nativeEvent.offsetY;
+    this.setState({
+      x1: this.x1,
+      x2: this.x2,
+      y1: this.y1,
+      y2: this.y2
+    });
+  }
+
+  getXY(e) {
+    const { offsetX, offsetY } = e.nativeEvent;
+    const { width, height } = this.state;
+    const x = Math.floor((offsetX / width) * 886);
+    const y = Math.floor((offsetY / height) * 716);
+    return { x, y };
+  }
   render() {
     const { plane, depth } = this.props;
-    const { zData } = this.state;
+    const { zData, width, height, x1, x2, y1, y2 } = this.state;
+    const className = "figurePosition";
+    const lineCoors = [[x1, y1], [x2, y2]];
     return (
       <React.Fragment>
-        <img
-          src={`./imgs/${plane}/${depth}.png`}
-          alt="Matrix"
-          ref={this.figureRef}
-          onClick={this.onClick}
-        />
+        <div>
+          <img
+            src={`./imgs/${plane}/${depth}.png`}
+            alt="Matrix"
+            className={className}
+            ref={this.figureRef}
+            onClick={this.onClick}
+            onMouseDown={this.onMouseDown}
+            onMouseMove={this.onMouseMove}
+            onMouseUp={this.onMouseUp}
+          />
+          <FigureSvgLayer
+            className={className}
+            width={width}
+            height={height}
+            lineCoors={lineCoors}
+          />
+        </div>
         <PolylineSvg zData={zData} width={1000} height={200} />
       </React.Fragment>
     );
