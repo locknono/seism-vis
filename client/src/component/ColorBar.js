94b5aa555da27fdc4036fad0a9a54731164e4c87
locknono <guozy0103@outlook.com>
2018-10-31 11:51:20 +0800

    Add error boundary component

---
+++ client/src/component/ColorBar.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class ColorBar extends Component {
+    render() {
+        return (
+            <div>
+                
+            </div>
+        );
+    }
+}
+
+export default ColorBar;
---
+++ client/src/error-boundary/ErrorBoundary.js
@@ -0,0 +1,20 @@
+import React, { Component } from "react";
+
+class ErrorBoundary extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { hasError: false };
+  }
+  static getDerivedStateFromError(error) {
+    return { hasError: true };
+  }
+  componentDidCatch(error, info) {}
+  render() {
+    if (this.state.hasError) {
+      return <h3>Something went wrong.</h3>;
+    }
+    return this.props.children;
+  }
+}
+
+export default ErrorBoundary;
