6ca3001e476c3912febc8394d8fe5f0e92c322a9
locknono <guozy0103@outlook.com>
2018-11-14 23:55:26 +0800

    Use imgOnLoad instead of componentDidMount

--- client/src/component/MatrixFigureV2.js
+++ client/src/component/MatrixFigureV2.js
@@ -35,32 +35,44 @@ class MatrixFigureV2 extends Component<Props, State> {
   constructor(props) {
     super(props);
     this.figureRef = React.createRef();
+    this.imgOnLoad = this.imgOnLoad.bind(this);
   }
 
-  componentDidMount() {
+  /*   componentDidMount() {
     const { xStart, xEnd, yStart, yEnd } = this.props;
-    console.log('xStart: ', xStart);
     setTimeout(
       function() {
         const figuerNode = this.figureRef.current;
         const { width, height, left, top } = figuerNode.getBoundingClientRect();
         this.props.changeSizePosition(width, height, left, top);
-        console.log("width: ", width);
-        console.log("xStart: ", xStart);
         const xScaler = d3
           .scaleLinear()
           .domain([xStart, xEnd])
           .range([0, width]);
-        console.log(xScaler(100000));
         const yScaler = d3
           .scaleLinear()
           .domain([yStart, yEnd])
           .range([height, 0]);
-
         this.props.getScaler({ xScaler, yScaler });
       }.bind(this),
       0
     );
+  } */
+
+  imgOnLoad() {
+    const { xStart, xEnd, yStart, yEnd } = this.props;
+    const figuerNode = this.figureRef.current;
+    const { width, height, left, top } = figuerNode.getBoundingClientRect();
+    this.props.changeSizePosition(width, height, left, top);
+    const xScaler = d3
+      .scaleLinear()
+      .domain([xStart, xEnd])
+      .range([0, width]);
+    const yScaler = d3
+      .scaleLinear()
+      .domain([yStart, yEnd])
+      .range([height, 0]);
+    this.props.getScaler({ xScaler, yScaler });
   }
 
   render() {
@@ -72,6 +84,7 @@ class MatrixFigureV2 extends Component<Props, State> {
           src={`./imgs/${planeName}/${depth}.png`}
           alt="matrix-figure"
           ref={this.figureRef}
+          onLoad={this.imgOnLoad}
           style={style}
         />
       </div>
