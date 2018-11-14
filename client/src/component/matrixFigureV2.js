d6384fc48fcd7f503598ce05dc0939bcdc9dab6d
locknono <guozy0103@outlook.com>
2018-11-14 11:57:28 +0800

    Connect matrixFigure component

--- client/src/component/matrixFigureV2.js
+++ client/src/component/matrixFigureV2.js
@@ -1,20 +1,50 @@
-//@flow
 import React, { Component } from "react";
 import { connect } from "react-redux";
-function mapStateToProps(state, ownProps) {
+import { changePlane, changeDepth } from "../action/changeImgPara";
+
+const mapStateToProps = (state, ownProps) => {
   const { planeName, depth } = state;
   return { planeName, depth };
-}
+};
 
-class MatrixFigureV2 extends Component {
+const mapDispatchToProps = {
+  changePlane,
+  changeDepth
+};
+
+class MatrixFigureV2 extends Component<Props, State> {
+  constructor(props) {
+    super(props);
+    this.figureRef = React.createRef();
+  }
+
+  componentDidMount() {
+    setTimeout(
+      function() {
+        const figuerNode = this.figureRef.current;
+        const { width, height } = figuerNode;
+        const { top, left } = figuerNode.getBoundingClientRect();
+      }.bind(this),
+      0
+    );
+  }
+
   render() {
     const { planeName, depth } = this.props;
+
     return (
       <div className="matrix-figure panel panel-default">
-        <img src={`./imgs/${planeName}/${depth}.png`} alt="matrix-figure" />
+        <img
+          src={`./imgs/${planeName}/${depth}.png`}
+          alt="matrix-figure"
+          ref={this.figureRef}
+        />
       </div>
     );
   }
 }
 
-export default connect(mapStateToProps)(MatrixFigureV2);
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(MatrixFigureV2);
