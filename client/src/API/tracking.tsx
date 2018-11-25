eeebf7ed786245c4385ada0e5141708f143dff88
locknono <guozy0103@outlook.com>
2018-11-25 14:52:42 +0800

    Extract tracking logic to seperate API file

---
+++ client/src/API/tracking.tsx
@@ -0,0 +1,150 @@
+export function clearSawtooth(
+  path: [number, number][],
+  x: number,
+  ifPositive: boolean
+): [number, number][] {
+  const deleteIndicesSet = new Set();
+  if (ifPositive) {
+    for (let i = 1; i < path.length - 2; i += 2) {
+      if (path[i][0] > x && path[i + 2][0] > x) {
+        deleteIndicesSet.add(i + 1);
+      }
+    }
+  } else {
+    for (let i = 1; i < path.length - 2; i += 2) {
+      if (path[i][0] < x && path[i + 2][0] < x) {
+        deleteIndicesSet.add(i + 1);
+      }
+    }
+  }
+  const deleteIndicesList = Array.from(deleteIndicesSet).reverse();
+  for (let i = 0; i < deleteIndicesList.length; i++) {
+    path.splice(deleteIndicesList[i], 1);
+  }
+  return path;
+}
+
+export function extractPeaks(positivePath: [number, number][], x: number) {
+  let peaks = [];
+  let peakPoints = [];
+  let findFlag = false;
+  for (let i = 0; i < positivePath.length - 1; i++) {
+    let point = positivePath[i];
+    let nextPoint = positivePath[i + 1];
+    if (point[0] === x && nextPoint[0] !== x) findFlag = true;
+    if (point[0] !== x && nextPoint[0] === x) findFlag = false;
+    if (findFlag === true) {
+      peakPoints.push(point);
+    } else if (findFlag === false && peakPoints.length > 0) {
+      let peakInfo = {
+        x: 0,
+        pos: -1,
+        value: -1
+      };
+      peakPoints.map(e => {
+        if (e[0] > peakInfo.x) {
+          peakInfo.x = e[0];
+          peakInfo.pos = e[1];
+          peakInfo.value = e[0] - x;
+        }
+      });
+      let peak = {
+        top: peakPoints[0][1],
+        bottom: peakPoints[peakPoints.length - 1][1],
+        mid: (peakPoints[0][1] + peakPoints[peakPoints.length - 1][1]) / 2,
+        peak: peakInfo.pos,
+        x: peakInfo.x,
+        value: peakInfo.value
+      };
+      peaks.push(peak);
+      peakPoints = [];
+    }
+  }
+  return peaks;
+}
+
+export function cutOffAllTracks(allTracks: any, traceCount: number) {
+  for (let i = allTracks.length - 1; i >= 0; i--) {
+    if (allTracks[i].length < traceCount / 2) {
+      allTracks.splice(i, 1);
+    }
+  }
+  let removeSet = new Set();
+  for (let i = 0; i < allTracks.length; i++) {
+    labelStop: for (let j = 0; j < allTracks.length; j++) {
+      if (i === j) continue;
+      let track1 = allTracks[i];
+      let track2 = allTracks[j];
+      if (track1.length === track2.length) continue;
+      for (let s = 1; s < track1.length; s++) {
+        for (let m = 1; m < track2.length; m++) {
+          if (
+            track1[s].mid === track2[m].mid &&
+            track1[s].x === track2[m].x &&
+            track1[s - 1].mid !== track2[m - 1].mid
+          ) {
+            if (track1.length < track2.length) {
+              removeSet.add(i);
+              break labelStop;
+            } else {
+              removeSet.add(j);
+              break labelStop;
+            }
+          }
+        }
+      }
+    }
+  }
+  let removeList = Array.from(removeSet).sort((a, b) => b - a);
+  for (let i = 0; i < removeList.length; i++) {
+    allTracks.splice(removeList[i], 1);
+  }
+}
+
+export function tracking(allPeaks: any, startTrackNumber: number) {
+  const allTracks = [];
+  for (let i = 0; i < allPeaks[startTrackNumber].length; i++) {
+    let track = [allPeaks[startTrackNumber][i]];
+    for (let j = startTrackNumber + 1; j < allPeaks.length; j++) {
+      let nextPeak = null;
+      let MaxOffSet = 999;
+      for (let s = 0; s < allPeaks[j].length; s++) {
+        let offset = Math.abs(allPeaks[j][s].mid - track[track.length - 1].mid);
+        if (offset < MaxOffSet) {
+          MaxOffSet = offset;
+          nextPeak = allPeaks[j][s];
+        }
+      }
+      //if offsets too much,stop here
+      if (MaxOffSet > 50) break;
+      track.push(nextPeak);
+    }
+    allTracks.push(track);
+  }
+  cutoff(allTracks);
+  return allTracks;
+
+  function cutoff(allTracks: any[]) {
+    for (let i = 0; i < allTracks.length - 1; i++) {
+      for (let j = 0; j < allTracks[i].length; j++) {
+        if (!allTracks[i + 1][j]) continue;
+        let curTrack = allTracks[i];
+        let nextTrack = allTracks[i + 1];
+        if (curTrack[j].mid !== nextTrack[j].mid) continue;
+        let curOffSet = Math.abs(curTrack[j].mid - curTrack[j - 1].mid);
+        let nextOffSet = Math.abs(nextTrack[j].mid - nextTrack[j - 1].mid);
+        let curValueDiff = Math.abs(curTrack[j].value - curTrack[j - 1].value);
+        let nextValueDiff = Math.abs(
+          nextTrack[j].value - nextTrack[j - 1].value
+        );
+        let curDiff = 0.5 * curOffSet + 0.5 * curValueDiff;
+        let nextDiff = 0.5 * nextOffSet + 0.5 * nextValueDiff;
+        if (curDiff < nextDiff) {
+          allTracks[i + 1].splice(j, allTracks[i + 1].length - j);
+        } else {
+          allTracks[i].splice(j, allTracks[i].length - j);
+        }
+      }
+    }
+  }
+}
--- client/src/component/WellMatch.tsx
+++ client/src/component/WellMatch.tsx
@@ -8,6 +8,12 @@ import {
 } from "../action/changeWell";
 import { changeSvgSize } from "../action/changeWellMatchSvg";
 import * as d3 from "d3";
+import {
+  clearSawtooth,
+  cutOffAllTracks,
+  extractPeaks,
+  tracking
+} from "../API/tracking";
 const mapStateToProps = (state: any, ownProps?: any) => {
   const {
     wellMinDepth,
@@ -98,7 +104,7 @@ class WellMatch extends React.Component<Props, State> {
         .line()
         .x(d => d[0])
         .y(d => d[1])
-       //.curve(d3.curveCardinal)
+      //.curve(d3.curveCardinal)
     };
     this.drawMatch = this.drawMatch.bind(this);
     this.drawTrace = this.drawTrace.bind(this);
@@ -126,11 +132,10 @@ class WellMatch extends React.Component<Props, State> {
       getTracePath,
       getAllTrack
     } = this.props;
-
     const drawWidth = width * (1 - 2 * paddingRatio);
     const pad = drawWidth / matrixData.length;
     //set xOffsetScope a litter bigger than `pad/2`
-    const xOffsetScope = pad / 1.3;
+    const xOffsetScope = pad / 0.8;
     const xScale = d3
       .scaleLinear()
       .domain([-13685.379, 13685.379])
@@ -163,8 +168,8 @@ class WellMatch extends React.Component<Props, State> {
         }
       }
 
-      positivePath = deleteMidPoint(positivePath, x, true);
-      negativePath = deleteMidPoint(negativePath, x, false);
+      positivePath = clearSawtooth(positivePath, x, true);
+      negativePath = clearSawtooth(negativePath, x, false);
 
       let peaks = extractPeaks(positivePath, x);
       allPeaks.push(peaks);
@@ -179,167 +184,10 @@ class WellMatch extends React.Component<Props, State> {
     for (let i = 0; i < allPeaks.length / 2; i++) {
       allTracks.push(...tracking(allPeaks, i));
     }
-
     cutOffAllTracks(allTracks, allPeaks.length);
     getAllTrack(allTracks);
-
     paths.push(positivePaths, negativePaths);
     getTracePath(paths);
-
-    function deleteMidPoint(
-      path: [number, number][],
-      x: number,
-      ifPositive: boolean
-    ): [number, number][] {
-      const deleteIndicesSet = new Set();
-      if (ifPositive) {
-        for (let i = 1; i < path.length - 2; i += 2) {
-          if (path[i][0] > x && path[i + 2][0] > x) {
-            deleteIndicesSet.add(i + 1);
-          }
-        }
-      } else {
-        for (let i = 1; i < path.length - 2; i += 2) {
-          if (path[i][0] < x && path[i + 2][0] < x) {
-            deleteIndicesSet.add(i + 1);
-          }
-        }
-      }
-      const deleteIndicesList = Array.from(deleteIndicesSet).reverse();
-      for (let i = 0; i < deleteIndicesList.length; i++) {
-        path.splice(deleteIndicesList[i], 1);
-      }
-      return path;
-    }
-
-    function tracking(allPeaks: any, original: number) {
-      const allTracks = [];
-      for (let i = 0; i < allPeaks[original].length; i++) {
-        let track = [allPeaks[original][i]];
-        for (let j = original + 1; j < allPeaks.length; j++) {
-          let nextPeak = null;
-          let MaxOffSet = 999;
-          for (let s = 0; s < allPeaks[j].length; s++) {
-            let offset = Math.abs(
-              allPeaks[j][s].mid - track[track.length - 1].mid
-            );
-            if (offset < MaxOffSet) {
-              MaxOffSet = offset;
-              nextPeak = allPeaks[j][s];
-            }
-          }
-          //if offsets too much,stop here
-          if (MaxOffSet > 50) break;
-          track.push(nextPeak);
-        }
-        allTracks.push(track);
-      }
-      cutoff(allTracks);
-      return allTracks;
-
-      function cutoff(allTracks: any[]) {
-        for (let i = 0; i < allTracks.length - 1; i++) {
-          for (let j = 0; j < allTracks[i].length; j++) {
-            if (!allTracks[i + 1][j]) continue;
-            let curTrack = allTracks[i];
-            let nextTrack = allTracks[i + 1];
-            if (curTrack[j].mid !== nextTrack[j].mid) continue;
-            let curOffSet = Math.abs(curTrack[j].mid - curTrack[j - 1].mid);
-            let nextOffSet = Math.abs(nextTrack[j].mid - nextTrack[j - 1].mid);
-            let curValueDiff = Math.abs(
-              curTrack[j].value - curTrack[j - 1].value
-            );
-            let nextValueDiff = Math.abs(
-              nextTrack[j].value - nextTrack[j - 1].value
-            );
-            let curDiff = 0.5 * curOffSet + 0.5 * curValueDiff;
-            let nextDiff = 0.5 * nextOffSet + 0.5 * nextValueDiff;
-            if (curDiff < nextDiff) {
-              allTracks[i + 1].splice(j, allTracks[i + 1].length - j);
-            } else {
-              allTracks[i].splice(j, allTracks[i].length - j);
-            }
-          }
-        }
-      }
-    }
-
-    function cutOffAllTracks(allTracks: any, traceCount: number) {
-      for (let i = allTracks.length - 1; i >= 0; i--) {
-        if (allTracks[i].length < traceCount / 2) {
-          allTracks.splice(i, 1);
-        }
-      }
-      let removeSet = new Set();
-      for (let i = 0; i < allTracks.length; i++) {
-        labelStop: for (let j = 0; j < allTracks.length; j++) {
-          if (i === j) continue;
-          let track1 = allTracks[i];
-          let track2 = allTracks[j];
-          if (track1.length === track2.length) continue;
-          for (let s = 1; s < track1.length; s++) {
-            for (let m = 1; m < track2.length; m++) {
-              if (
-                track1[s].mid === track2[m].mid &&
-                track1[s].x === track2[m].x &&
-                track1[s - 1].mid !== track2[m - 1].mid
-              ) {
-                if (track1.length < track2.length) {
-                  removeSet.add(i);
-                  break labelStop;
-                } else {
-                  removeSet.add(j);
-                  break labelStop;
-                }
-              }
-            }
-          }
-        }
-      }
-      let removeList = Array.from(removeSet).sort((a, b) => b - a);
-      for (let i = 0; i < removeList.length; i++) {
-        allTracks.splice(removeList[i], 1);
-      }
-    }
-
-    function extractPeaks(positivePath: [number, number][], x: number) {
-      let peaks = [];
-      let peakPoints = [];
-      let findFlag = false;
-      for (let i = 0; i < positivePath.length - 1; i++) {
-        let point = positivePath[i];
-        let nextPoint = positivePath[i + 1];
-        if (point[0] === x && nextPoint[0] !== x) findFlag = true;
-        if (point[0] !== x && nextPoint[0] === x) findFlag = false;
-        if (findFlag === true) {
-          peakPoints.push(point);
-        } else if (findFlag === false && peakPoints.length > 0) {
-          let peakInfo = {
-            x: 0,
-            pos: -1,
-            value: -1
-          };
-          peakPoints.map(e => {
-            if (e[0] > peakInfo.x) {
-              peakInfo.x = e[0];
-              peakInfo.pos = e[1];
-              peakInfo.value = e[0] - x;
-            }
-          });
-          let peak = {
-            top: peakPoints[0][1],
-            bottom: peakPoints[peakPoints.length - 1][1],
-            mid: (peakPoints[0][1] + peakPoints[peakPoints.length - 1][1]) / 2,
-            peak: peakInfo.pos,
-            x: peakInfo.x,
-            value: peakInfo.value
-          };
-          peaks.push(peak);
-          peakPoints = [];
-        }
-      }
-      return peaks;
-    }
   }
 
   drawMatch() {
@@ -404,7 +252,6 @@ class WellMatch extends React.Component<Props, State> {
               path.push([x, null]);
             }
           }
-          console.log("path: ", path);
           paths.push(path);
         }
         for (let i = 0; i < paths.length; i++) {
@@ -424,14 +271,14 @@ class WellMatch extends React.Component<Props, State> {
         for (let i = 0; i < paths.length; i++) {
           paths[i].push(paths[i][0]);
         }
-        console.log("paths: ", paths);
         getWellCurve(paths);
       });
   }
 
+  calUncertainty() {}
+
   render() {
     const { width, height, curvePaths, paths, allTrack } = this.props;
-
     const { colorScale, pathGen } = this.state;
     let curves = null;
     if (curvePaths) {
