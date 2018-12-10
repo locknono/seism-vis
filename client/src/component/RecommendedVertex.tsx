263261890f4d79446d7130615b768fe9e7bdea6b
locknono <guozy0103@outlook.com>
2018-12-10 23:28:32 +0800

    Draw recommend rect

--- client/src/component/RecommendedVertex.tsx
+++ client/src/component/RecommendedVertex.tsx
@@ -1,8 +1,12 @@
 import * as React from "react";
 import { VertexType } from "src/ts/Type";
 import { v4 } from "uuid";
-import { vertexRadius, vertexFillOpacity } from "../constraint";
-
+import {
+  vertexRadius,
+  vertexFillOpacity,
+  darkerMatchColor
+} from "../constraint";
+import * as d3 from "d3";
 interface Props {
   vertex: VertexType | undefined;
   changeVertexPosition: any;
@@ -22,6 +26,8 @@ class RecommendedVertex extends React.Component<Props> {
   render() {
     const { vertex, originalVertex } = this.props;
     let drawVertex = null;
+    let recommendRect = null;
+    let recommendBaseLine = null;
     if (vertex) {
       drawVertex = vertex.map((e, i) => {
         if (e[0] === originalVertex[i][0] && e[1] === originalVertex[i][1]) {
@@ -42,9 +48,57 @@ class RecommendedVertex extends React.Component<Props> {
           />
         );
       });
+      const path = d3.path();
+      path.moveTo(vertex[0][0], vertex[0][1]);
+      path.lineTo(vertex[1][0], vertex[1][1]);
+      path.lineTo(vertex[3][0], vertex[3][1]);
+      path.lineTo(vertex[2][0], vertex[2][1]);
+      path.lineTo(vertex[0][0], vertex[0][1]);
+      recommendRect = (
+        <path
+          d={path.toString()}
+          style={{ fill: darkerMatchColor, fillOpacity: 0.8 }}
+        />
+      );
+      recommendBaseLine = getBaseLine(vertex);
     }
-    return <React.Fragment>{drawVertex}</React.Fragment>;
+
+    return (
+      <React.Fragment>
+        {recommendRect} {drawVertex}
+        {recommendBaseLine}
+      </React.Fragment>
+    );
   }
 }
 
 export default RecommendedVertex;
+
+function getBaseLine(vertex: VertexType) {
+  let baseLineStyle = {
+    stroke: darkerMatchColor,
+    fill: darkerMatchColor,
+    fillOpacity: 0.6
+  };
+
+  const lPath = d3.path();
+  lPath.moveTo(vertex[0][0], vertex[0][1]);
+  lPath.lineTo(0, vertex[0][1]);
+  lPath.lineTo(0, vertex[1][1]);
+  lPath.lineTo(vertex[0][0], vertex[1][1]);
+
+  const rPath = d3.path();
+  rPath.moveTo(vertex[2][0], vertex[2][1]);
+  rPath.lineTo(700, vertex[2][1]);
+  rPath.lineTo(700, vertex[3][1]);
+  rPath.lineTo(vertex[3][0], vertex[3][1]);
+
+  const leftPath = (
+    <path d={lPath.toString()} key={lPath.toString()} style={baseLineStyle} />
+  );
+  const rightPath = (
+    <path d={rPath.toString()} key={rPath.toString()} style={baseLineStyle} />
+  );
+
+  return [leftPath, rightPath];
+}
