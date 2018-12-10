7f2115af1fb5dae7ac1e5fd81877187aa117cd7e
locknono <guozy0103@outlook.com>
2018-12-10 17:50:43 +0800

    Change infopanel to class component

--- client/src/component/InfoPanel/InfoPanel.tsx
+++ client/src/component/InfoPanel/InfoPanel.tsx
@@ -3,6 +3,13 @@ import { connect } from "react-redux";
 import { Panel } from "react-bootstrap";
 import { WellCircle } from "./WellCircle";
 import { ViewHeading } from "../ViewHeading";
+import { CoupleWell } from "src/ts/Type";
+import * as $ from "jquery";
+interface Props {
+  planeName: string;
+  depth: number;
+  coupleWell: CoupleWell;
+}
 const mapStateToProps = (state: any, ownProps?: any) => {
   const { planeName, depth } = state.figReducer;
   const { xStart, yStart, xEnd, yEnd } = state.globalVarReducer;
@@ -10,15 +17,44 @@ const mapStateToProps = (state: any, ownProps?: any) => {
   return { planeName, depth, xStart, yStart, xEnd, yEnd, coupleWell };
 };
 
-const InfoPanel = (props: any) => {
-  const { planeName, depth, coupleWell } = props;
-  return (
-    <div className="info-panel panel panel-primary">
-      <ViewHeading height={22} title={`Info`} />
-      <div />
-      <WellCircle coupleWell={coupleWell} />
-    </div>
-  );
-};
+class InfoPanel extends React.Component<Props> {
+  constructor(props: Props) {
+    super(props);
+  }
+
+  componentDidMount() {
+    ($("#ex1") as any).slider({
+      formatter: function(value: any) {
+        return "Current value: " + value;
+      }
+    });
+    var slider = new Slider("#ex1", {
+      formatter: function(value) {
+        return "Current value: " + value;
+      }
+    });
+  }
+  render() {
+    const { planeName, depth, coupleWell } = this.props;
+    return (
+      <div className="info-panel panel panel-primary">
+        <ViewHeading height={22} title={`Info`} />
+        <div />
+        <WellCircle coupleWell={coupleWell} />
+        <div>
+          <input
+            id="ex1"
+            data-slider-id="ex1Slider"
+            type="text"
+            data-slider-min="0"
+            data-slider-max="20"
+            data-slider-step="1"
+            data-slider-value="14"
+          />
+        </div>
+      </div>
+    );
+  }
+}
 
 export default connect(mapStateToProps)(InfoPanel);
